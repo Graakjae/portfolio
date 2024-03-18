6447cc8f9d9b4f573a43285067380657868c8392
Frederik Graakjær <graakjaer.f@gmail.com>
2024-03-18 11:05:53 +0100

    page analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@
 import { Lato } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
+import { Analytics } from "@vercel/analytics/react";
 
 const lato = Lato({
     weight: ["400", "700", "900"],
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
             <body className={`${lato.className} bg-[#E3E3DE]`}>
                 <Nav />
                 {children}
+                <Analytics />
             </body>
         </html>
     );
