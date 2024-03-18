df6fbb9cc81a35bdfe76e37e5e293c4f157a8829
Frederik Graakjær <graakjaer.f@gmail.com>
2024-03-18 11:17:51 +0100

    remove page analytics

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@
 import { Lato } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
-import { Analytics } from "@vercel/analytics/react";
 
 const lato = Lato({
     weight: ["400", "700", "900"],
@@ -15,7 +14,6 @@ export default function RootLayout({ children }) {
             <body className={`${lato.className} bg-[#E3E3DE]`}>
                 <Nav />
                 {children}
-                <Analytics />
             </body>
         </html>
     );
