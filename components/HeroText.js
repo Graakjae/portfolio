5dcc9d2a4a96eac0f3b7af86c76defb157e06f24
Frederik Graakjær <frede.flip@gmail.com>
2024-11-07 10:20:03 +0100

    Test

--- components/HeroText.js
+++ components/HeroText.js
@@ -1,23 +1,23 @@
 export default function HeroText({ reference, isSecondText }) {
-    return (
-        <span
-            className={`m-0 block text-[#9F8B78] uppercase lg:text-[140px] font-bold pr-[30px] tracking-[4.8px] md:tracking-[8.4px] md:text-[80px] text-[60px] ${
-                isSecondText && "absolute left-[100%] top-0"
-            }`}
-            ref={reference}
-        >
-            <Paragraph text="frederik graakjær" />
-            <Paragraph text="frontend developer" />
-            <Paragraph text="frederik graakjær" />
-            <Paragraph text="frontend developer" />
-        </span>
-    );
+  return (
+    <span
+      className={`m-0 block text-[#9F8B78] uppercase lg:text-[140px] font-bold pr-[30px] tracking-[4.8px] md:tracking-[8.4px] md:text-[80px] text-[60px] ${
+        isSecondText && "absolute left-[100%] top-0"
+      }`}
+      ref={reference}
+    >
+      <Paragraph text="Frederik Graakjær" />
+      <Paragraph text="frontend developer" />
+      <Paragraph text="Frederik Graakjær" />
+      <Paragraph text="frontend developer" />
+    </span>
+  );
 }
 
 export function Paragraph({ text }) {
-    return (
-        <>
-            {text} <span className={`text-[#9F8B78] transition-colors`}>-</span>{" "}
-        </>
-    );
+  return (
+    <>
+      {text} <span className={`text-[#9F8B78] transition-colors`}>-</span>{" "}
+    </>
+  );
 }
